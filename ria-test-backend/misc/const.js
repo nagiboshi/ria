1e9f8da97cdef371c1639497d4cd5783a37a26df
Alexdev <afanasiev.uae@gmail.com>
2018-06-25 16:44:08 +0400

    const

--- ria-test-backend/misc/const.js
+++ ria-test-backend/misc/const.js
@@ -1,9 +1,7 @@
-const gmail_user = 'ria.test.acc@gmail.com';
-const gmail_password = 'u8kKoJI8yoKWzZuLGXQl'
 class Consts { 
     // MAIL 
-    static get GMAIL_USER(){return gmail_user; };
-    static get GMAIL_PASSWORD(){return gmail_password; };
+    static get GMAIL_USER(){return 'ria.test.acc@gmail.com'; };
+    static get GMAIL_PASSWORD(){return 'u8kKoJI8yoKWzZuLGXQl'; };
 }
 
 module.exports = new Consts();
